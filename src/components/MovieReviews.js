8f027d660038a6bd3430e91050a0a87ccd72317d
Justin Winger <justinwinger4@gmail.com>
2020-07-29 08:56:59 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props) {
+    return (
+        <div className="review-list">
+            {props.reviews.map(review => {
+                return ( 
+                    <div className="review">
+                        <h1>{review.headline}</h1>
+                        <p>{review.summary_short}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
